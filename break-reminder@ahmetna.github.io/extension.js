145dc16223fa5b257212ff9d69243727a46ad9fa
= <=>
2026-01-05 20:20:08 +0300

    feat: Add media pausing functionality before displaying the break screen.

--- break-reminder@ahmetna.github.io/extension.js
+++ break-reminder@ahmetna.github.io/extension.js
@@ -1,6 +1,6 @@
 import GLib from 'gi://GLib';
 import Gio from 'gi://Gio';
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
 export default class BreakReminderExtension extends Extension {
     constructor(metadata) {
@@ -14,12 +14,12 @@ export default class BreakReminderExtension extends Extension {
     enable() {
         this._scriptPath = this.path + '/break_screen.py';
         this._settings = this.getSettings();
-        
+
         // Settings değişikliğini dinle
         this._settingsChangedId = this._settings.connect('changed::break-duration', () => {
             this._startTimer();
         });
-        
+
         this._startTimer();
     }
 
@@ -29,13 +29,13 @@ export default class BreakReminderExtension extends Extension {
             GLib.Source.remove(this._timeoutId);
             this._timeoutId = null;
         }
-        
+
         // Disconnect signal
         if (this._settingsChangedId) {
             this._settings.disconnect(this._settingsChangedId);
             this._settingsChangedId = null;
         }
-        
+
         // Cleanup settings reference
         this._settings = null;
     }
@@ -56,6 +56,9 @@ export default class BreakReminderExtension extends Extension {
 
     _showBreakScreen() {
         try {
+            // Pause media before showing break screen
+            this._pauseMedia();
+
             // Launch Python GTK application
             let proc = Gio.Subprocess.new(
                 ['python3', this._scriptPath],
@@ -70,6 +73,21 @@ export default class BreakReminderExtension extends Extension {
             logError(e, 'Failed to launch break screen');
             // Restart timer even on error
             this._startTimer();
+        }
+    }
+
+    _pauseMedia() {
+        try {
+            // Use playerctl to pause all media players
+            let proc = Gio.Subprocess.new(
+                ['playerctl', '-a', 'pause'],
+                Gio.SubprocessFlags.NONE
+            );
+
+            // We don't need to wait for this to finish
+        } catch (e) {
+            // Log error but don't let it stop the break screen from showing
+            logError(e, 'Failed to pause media');
         }
     }
 }
