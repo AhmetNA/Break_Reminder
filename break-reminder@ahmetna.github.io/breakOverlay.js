adf5aac09a4440288d95cef93cb100aa2e72c476
= <=>
2026-01-17 14:01:51 +0300

    bugfix: full screen view fixeed

--- break-reminder@ahmetna.github.io/breakOverlay.js
+++ break-reminder@ahmetna.github.io/breakOverlay.js
@@ -15,6 +15,11 @@ export const BreakOverlay = GObject.registerClass({
             reactive: true,
             can_focus: true,
             track_hover: true,
+            x_expand: true,
+            y_expand: true,
+            layout_manager: new Clutter.BinLayout(),
+            style_class: 'break-overlay',
+            style: 'background-color: rgba(51, 51, 51, 0.98);',
         });
 
         this._settings = settings;
@@ -22,32 +27,35 @@ export const BreakOverlay = GObject.registerClass({
         this._countdownId = null;
         this._grab = null;
 
-        // Ana container - tam ekran
+        // Main container - fullscreen
         this._container = new St.BoxLayout({
             style_class: 'break-overlay-container',
             vertical: true,
             x_expand: true,
             y_expand: true,
+            x_align: Clutter.ActorAlign.FILL,
+            y_align: Clutter.ActorAlign.FILL,
         });
 
         this.add_child(this._container);
 
-        // Üst spacer - ortalamak için
+        // Top spacer - for centering
         let topSpacer = new St.Widget({ y_expand: true });
         this._container.add_child(topSpacer);
 
-        // İçerik box'ı
+        // Content box
         let contentBox = new St.BoxLayout({
             style_class: 'break-overlay-content',
             vertical: true,
             x_align: Clutter.ActorAlign.CENTER,
+            style: 'spacing: 30px;',
         });
         this._container.add_child(contentBox);
 
-        // Ana mesaj
+        // Main message
         this._messageLabel = new St.Label({
             style_class: 'break-overlay-message',
-            text: 'Gözlerin ve Sırtın İçin Mola Ver!\nDerin bir nefes al.',
+            text: 'Take a Break for Your Eyes and Back!\nTake a deep breath.',
         });
         this._messageLabel.clutter_text.set_line_alignment(2); // Center
         contentBox.add_child(this._messageLabel);
@@ -59,22 +67,22 @@ export const BreakOverlay = GObject.registerClass({
         });
         contentBox.add_child(this._countdownLabel);
 
-        // Alt spacer
+        // Bottom spacer
         let bottomSpacer = new St.Widget({ y_expand: true });
         this._container.add_child(bottomSpacer);
 
-        // Buttons box - en altta
+        // Buttons box - at the bottom
         let closeBox = new St.BoxLayout({
             x_align: Clutter.ActorAlign.CENTER,
             style_class: 'break-overlay-close-box',
             vertical: true,
-            spacing: 20
+            style: 'spacing: 20px;',
         });
         this._container.add_child(closeBox);
 
         this._closeButton = new St.Button({
             style_class: 'break-overlay-close-button',
-            label: 'Molayı Bitir',
+            label: 'End Break',
             reactive: true,
             can_focus: true,
             track_hover: true,
@@ -92,7 +100,7 @@ export const BreakOverlay = GObject.registerClass({
         this._snoozeButton.connect('clicked', () => this._onSnooze());
         closeBox.add_child(this._snoozeButton);
 
-        // Klavye eventi için
+        // For keyboard events
         this.connect('key-press-event', (actor, event) => {
             let symbol = event.get_key_symbol();
             if (symbol === Clutter.KEY_Escape) {
@@ -104,58 +112,58 @@ export const BreakOverlay = GObject.registerClass({
     }
 
     show() {
-        // Ekrana ekle
+        // Add to screen
         Main.layoutManager.addChrome(this, {
             affectsStruts: false,
             trackFullscreen: true,
         });
 
-        // Tam ekran boyutuna ayarla
+        // Set to fullscreen size
         let monitor = Main.layoutManager.primaryMonitor;
         this.set_position(monitor.x, monitor.y);
         this.set_size(monitor.width, monitor.height);
 
-        // Input'u yakala
+        // Grab input
         this._grab = Main.pushModal(this, {
             actionMode: 1, // Shell.ActionMode.NORMAL
         });
 
-        // Fade-in animasyonu
+        // Fade-in animation
         this.opacity = 0;
         this.ease_property('opacity', 255, {
             duration: 300,
             mode: Clutter.AnimationMode.EASE_OUT_QUAD,
         });
 
-        // Focus al
+        // Grab focus
         this.grab_key_focus();
 
-        // Countdown başlat
+        // Start countdown
         this._startCountdown();
     }
 
     hide() {
-        // Countdown'u durdur
+        // Stop countdown
         if (this._countdownId) {
             GLib.Source.remove(this._countdownId);
             this._countdownId = null;
         }
 
-        // Fade-out animasyonu
+        // Fade-out animation
         this.ease_property('opacity', 0, {
             duration: 200,
             mode: Clutter.AnimationMode.EASE_OUT_QUAD,
             onComplete: () => {
-                // Input yakalamayı bırak
+                // Release input grab
                 if (this._grab) {
                     Main.popModal(this._grab);
                     this._grab = null;
                 }
 
-                // Ekrandan kaldır
+                // Remove from screen
                 Main.layoutManager.removeChrome(this);
 
-                // Parent widget'tan çıkar ve yok et
+                // Remove from parent widget and destroy
                 if (this.get_parent()) {
                     this.get_parent().remove_child(this);
                 }
